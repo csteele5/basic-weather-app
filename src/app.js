35f40a2cadaa076b0425c95fd7c45ecd7bdedb8c
Charles Steele <siegcw@yahoo.com>
2020-09-18 15:57:52 -0700

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ const airvisual = require('./utils/airvisual')
 const { response } = require('express')
 
 const app = express()
+// For Heroku deployment
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -177,6 +179,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port '+port)
 })
